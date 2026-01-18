05dfbda8a7b214f4866b6782d8001c8c12a58913
Yegor Kulik <egorkulik9696@gmail.com>
2026-01-18 19:07:19 +0500

    Fixes

--- apps/temperature-api/index.js
+++ apps/temperature-api/index.js
@@ -9,65 +9,73 @@ app.get('/health', (req, res) => {
     });
 });
 
-app.get('/temperature', (req, res) => {
-    const locationParam = req.query.location;
-    const sensorIDParam = req.query.sensorID;
+app.get('/', (req, res) => {
+    const { location } = req.query;
 
-    const temperature = Math.round(
-        (Math.random() * (30 - 10) + 10) * 10
-    ) / 10;
+    const SENSORS = {
+        "1": "Living Room",
+        "2": "Bedroom",
+        "3": "Kitchen",
+    };
 
-    let locationRes = "";
+    const resolvedLocation =
+        (typeof location === "string" && location.trim() !== "")
+            ? SENSORS[location] || "Unknown"
+            : "Unknown";
 
-    if (!locationParam || locationParam === "") {
-        switch (sensorIDParam) {
-            case "1":
-                locationRes = "Living Room"
-                break;
-            case "2":
-                locationRes = "Bedroom";
-                break;
-            case "3":
-                locationRes = "Kitchen";
-                break;
-            default:
-                locationRes = "Unknown";
-                break;
-        }
-    }
+    const temperature =
+        Math.round((Math.random() * (30 - 10) + 10) * 10) / 10;
 
-    let sensorIdRes = "";
-
-    if (!sensorIDParam || sensorIDParam === "") {
-        switch (sensorIDParam) {
-            case "1":
-                sensorIdRes = "Living Room"
-                break;
-            case "2":
-                sensorIdRes = "Bedroom";
-                break;
-            case "3":
-                sensorIdRes = "Kitchen";
-                break;
-            default:
-                sensorIdRes = "Unknown";
-                break;
-        }
-    }
-
-
-    res.json({
+    const resDTO = {
         value: temperature,
         unit: "°C",
-        timestamp: new Date().getDate(),
-        location: locationRes,
+        timestamp: new Date().toISOString(),
+        location: resolvedLocation,
         status: "active",
-        sensorID: sensorIdRes,
-        sensorType: "temperature",
-        description: `Temperature sensor: ${locationRes}`,
-    });
+        sensor_id: null,
+        sensor_type: "temperature",
+        description: `Temperature sensor: ${resolvedLocation}`,
+    };
+
+    res.json(resDTO);
 });
 
-app.listen(PORT, () => {
+app.get("/temperature/:sensorID", (req, res) => {
+    const { sensorID } = req.params;
+
+    const SENSORS = {
+        "1": "Living Room",
+        "2": "Bedroom",
+        "3": "Kitchen",
+    };
+
+    const resolverSensor =
+        (typeof sensorID === "string" && sensorID.trim() !== "")
+            ? SENSORS[sensorID] || "Unknown"
+            :  "Unknown";
+
+    const temperature =
+        Math.round((Math.random() * (30 - 10) + 10) * 10) / 10;
+
+    const resDTO = {
+        value: temperature,
+        unit: "°C",
+        timestamp: new Date().toISOString(),
+        location: resolverSensor,
+        status: "active",
+        sensor_id: sensorID || null,
+        sensor_type: "temperature",
+        description: `Temperature sensor: ${resolverSensor}`,
+    };
+
+    res.json(resDTO);
+})
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl} ${req.query}`);
+    next();
+});
+
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`Server listening on port ${PORT}`);
 });
